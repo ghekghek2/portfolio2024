191dd17ef3d6829e7751fde50a2ed14e0a7bf32c
Kevin Divinagracia <74808131+ghekghek2@users.noreply.github.com>
2024-03-10 15:11:10 +0800

    install react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import Pagelayout from "./ui/Pagelayout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
+import AppLayout from "./ui/AppLayout";
+
 function App() {
-  return <Pagelayout />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Pagelayout />}>
+          <Route index path="kevinDivinagracia" element={<AppLayout />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
