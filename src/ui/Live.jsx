a4cd71fda2ade9174e132d61b6c5e70d33da5e3f
Kevin Divinagracia <74808131+ghekghek2@users.noreply.github.com>
2024-03-15 15:49:34 +0800

    Responsive pin

--- src/ui/Live.jsx
+++ src/ui/Live.jsx
@@ -1,13 +1,19 @@
 import ProjectUi from "./ProjectUi";
 
 function Live() {
+  const projects = [
+    "../dashboard.png",
+    "../dashboard-dark.png",
+    // "../checkin.png",
+    // "../checkin-dark.png",
+    // "../booking-dark.png",
+  ];
+
   return (
-    <div>
-      <ProjectUi img="../gsap.png" title="Project 1" />
-      <ProjectUi img="../gsap.png" title="Project 2" />
-      <ProjectUi img="../gsap.png" title="Project 3" />
-      <ProjectUi img="../gsap.png" title="Project 4" />
-      <ProjectUi img="../gsap.png" title="Project 5" />
+    <div className="">
+      <ProjectUi des="des1" title="Project 1" img={projects} />
+      <ProjectUi des="des2" title="Project 2" img={projects} />
+      <ProjectUi des="des3" title="Project 3" img={projects} />
     </div>
   );
 }
